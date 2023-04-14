6d74ef45cf0219ed549f18263c95a4a3e6e2fcc4
junfminK1m <deliah07@naver.com>
2023-04-14 11:43:11 +0900

    상품재고보다 많이 구매 못하도록 하기 완료

--- src/main/java/shop/mtcoding/productapp_buyer/controller/OrderController.java
+++ src/main/java/shop/mtcoding/productapp_buyer/controller/OrderController.java
@@ -1,6 +1,5 @@
 package shop.mtcoding.productapp_buyer.controller;
 
-import java.security.Principal;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -11,11 +10,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import shop.mtcoding.productapp_buyer.dto.orders.OrdersDto;
 import shop.mtcoding.productapp_buyer.model.orders.Orders;
 import shop.mtcoding.productapp_buyer.model.orders.OrdersRepository;
+import shop.mtcoding.productapp_buyer.model.product.Product;
 import shop.mtcoding.productapp_buyer.model.product.ProductRepository;
 import shop.mtcoding.productapp_buyer.model.user.User;
 
@@ -56,23 +55,25 @@ public class OrderController {
         if (principal == null) {
             return "redirect:/";
         }
-        // System.out.println(ordersDto.getOrdersName());
+
+        // 상품수량보다 구매수량이 더 많으면 안됨
+        Product productPS = productRepository.findById(productId);
+        if (productPS.getProductQty() - ordersDto.getOrdersQty() < 0) {
+            return "redirect:/product/{productId}";
+        }
 
         // 구매를 하면 product qty가 차감되어야 함
         productRepository.productQtyUpdate(ordersDto);
 
-        // System.out.println("프로덕트 레파지토리 : " + ordersDto.getOrdersName());
-
         // principal.getUserId() 너무 길어서 변수로 만듦
         int userId = principal.getUserId();
+
         /*
          * 구매버튼 누르면 insert 됨
          * 누가 구매했는 지 필요하기 때문에 userId도 같이 insert 해야 함
          */
         ordersRepository.insert(ordersDto, userId);
-
-        // System.out.println("오더 레파지토리 : " + ordersDto.getOrdersName());
-
         return "redirect:/orderListForm/" + userId;
+
     }
 }
